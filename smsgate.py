fb82b28e2d3c5f164010b79039abefb2c4f1cf66
troll <shuric80@gmail.com>
2016-02-08 12:41:14 +0300

    release

--- smsgate.py
+++ smsgate.py
@@ -1,98 +1,58 @@
 #-*- coding:utf-8 -*-
-import sys
 import os
-from urllib import urlopen
-import json
+import requests
 import sqlite3
-from datetime import datetime
-import re
+import json
+import logging
 
-LOGIN = 'user'
-PASSWORD = 'password'
+logging.basicConfig(filename='smsgate.log',format='%(asctime)s %(message)s', level=logging.INFO)
+
 DEBUG= True
-"""
-В settings хранится json файл вида.
-{
- "имя_сервиса":{
-                "http":"https", #протокол
-                "url":"//sms.ru", #url-адрес
-                "script":"send.php" #
-                "message":"txt" #
-                "password":"psw",
-                "login":"login",
-                 "parametrs":{ #тут различные параметры для настройки
-                              "a":"a",
-                               .....
-                               "z":"z"
-                              }
-               }
-...
-}
-  """
 class SMSGate():
-    def __init__(self, json_arg, name):
-        # l - словарь со всеми настройками для сервиса.
-        self.l = json_arg
-        self.name_service = name
-        
-        db_exist = os.path.isfile('smsgate.db')
-        self._conn = sqlite3.connect('smsgate.db')
-        self._cur = self._conn.cursor()
-
-        if not db_exist:
-          self._cur.execute('''CREATE TABLE log_sms (date text, phone text, message text, ret text)''')
-          self._conn.commit()
-    
-    def send(self, phone, msg):
-        # param - читаются из json файла параметры и формируется строка.
-        param = '&'.join([ '='.join((key, value)) for key, value in self.l['parametrs'].items()])
-        # формируется url 
-        url = self.l['http'] + self.l['url'] +'/'+ self.l['script']
-        # аргументы для POST
-        ls_arg =  [
-            [self.l['password'],PASSWORD],
-            [self.l['login'],LOGIN],
-            [self.l['message'],msg],
-            [self.l['phone'],str(phone)]
+    def __init__(self, arg, service):
+        self.arg = arg
+        self.service = service
             
-        ]
-        arg = '&'.join(['='.join(i) for i in ls_arg ])
-        arg = arg+'&'+param
-        # для отладки записываю в файл и эмулируем ответ.
-        if DEBUG:
-            f =open('debug','a')
-            f.write('URL:%s\tPOST:%s'%(url,arg))
-            f.write('\n')
-            f.close()
-            xml_ret = "<sms><status>status</status><last_date>last_date</last_date><last_timestamp>last_timestamp</last_timestamp><err>err</err></sms>"
-        else:
-            xml_ret = urlopen(url, arg.encode(CHARSET)).data()
-        #парсим
-        ret_dict = re.findall('(\w+)>(\w+)</',xml_ret)
-        # превращаем в json формат
-        ret = json.dumps(dict(ret_dict))
-        # логгируем в БД
-        self.log(phone,msg,ret)
+    def send(self, phone, msg):
+        # api указанное на сайте и  api  в задании,они отличаются,
+        # поэтому беру данные  из задания.
+        d_output = {}
+        url = self.arg.get('URL')
+        post_data = dict()
+        post_data[self.arg['MESSAGE']] = msg   # У разных сервисов могут быть разные ключи для полей
+        post_data[self.arg['PHONE_NUMBER']] = phone #  сообщения и номера телефона. Поэтому их храним
+                                                    #  в конфиге для каждого случая.
+        post_data.update(self.arg['PARAMETRS'])     # добавляем параметры для сервиса в post запрос.
+        ret_obj = requests.post(self.arg['URL'], data = post_data)  # post -запрос.
+        ret = ret_obj.json()   # вытаскивает json объект из ответа.
+        logging.info("%s :%s: %s:%s "%(self.service, phone, msg, ret))
+        
         return ret
     
-    def log(self,phone, msg, ret):
-        log_data =(datetime.now(), phone, msg, ret)
-        self._cur.execute("INSERT INTO log_sms VALUES(?,?,?,?)",log_data)
-        self._conn.commit()
-        
+            
 def get_handler(handler_name):
     service=dict()
-    #читаем json файл
+    print 'load'
     with open('settings') as file:
         data = json.load(file)
-    # создаем объекты , заносим в них данные.Сами объекты собираем в словарь.
+    print data
+    
     for  key, value in data.items():
         service[key] = SMSGate(value, key)
-    #возвращаем объект по индексу. Если его нету, идет None    
+    
     ret = service.get(handler_name,None)
     return ret
 
-            
+if __name__ == '__main__':
+    
+      print "\
+         #SMS GATE: Example.\n \
+         import smsgate \n \
+         \n \
+         handler = smsgate.get_handler('smsc') \n \
+         handler.send('+7926XXXXXXX','Hello world!')"
+       
+
         
             
         
